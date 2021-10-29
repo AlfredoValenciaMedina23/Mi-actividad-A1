0378206d6602cdfffa75ee1254b26351ce6ca368
LeomarVM <92315857+LeomarValencia@users.noreply.github.com>
2021-10-29 10:12:20 -0500

    Update Suma.cpp

--- Suma.cpp
+++ Suma.cpp
@@ -1,14 +1,14 @@
 // PROGRAMA DE SUMA DE DOS MUMEROS
 // ===========================
-# incluye < iostream >
-usando el  espacio de nombres  std ;
+# include<iostream>
+using namespace std ;
 int  main ()
 {
-int a, b, c;
-a = 3 ;
-b = 4 ;
-c = a + b;
+int a,b,c;
+a=3 ;
+b=4 ;
+c=a+b;
 
-cout << " eL RESULTADO ES: " << c << endl;
+cout << " EL RESULTADO ES: " << c << endl;
 return  0 ;
 }
