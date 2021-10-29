ca30d55443d0377ad56dd33c0c80fbc5e59fe5cf
LeomarVM <92315857+LeomarValencia@users.noreply.github.com>
2021-10-29 09:33:49 -0500

    Create Suma.cpp

---
+++ Suma.cpp
@@ -0,0 +1,14 @@
+// PROGRAMA DE SUMA DE DOS MUMEROS
+// ===========================
+# incluye < iostream >
+usando el  espacio de nombres  std ;
+int  main ()
+{
+int a, b, c;
+a = 3 ;
+b = 4 ;
+c = a + b;
+
+cout << " eL RESULTADO ES: " << c << endl;
+return  0 ;
+}
