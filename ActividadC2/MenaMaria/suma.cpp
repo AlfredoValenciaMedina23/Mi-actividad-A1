6413ba53f8e606cf9ceb4daaea2134f0afb0231e
Maria Mena <93356652+MariaMenaLino@users.noreply.github.com>
2021-12-18 00:49:45 -0500

    Add files via upload
    #include<iostream>
    using namespace std; 
    int main()
    {int x,y,z;
    cout<<"\n ingrese numero x:";
    cin>>x;
    cout<<"\n ingrese numero y:";
    cin>>y;
    z=x+y;
    cout<<"\n la suma de "<<x<<"+"<<y<<" es:"<<z;
    
    return 0;
    }

---
+++ ActividadC2/MenaMaria/suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+int main()
+{int x,y,z;
+cout<<"\n ingrese numero x:";
+cin>>x;
+cout<<"\n ingrese numero y:";
+cin>>y;
+z=x+y;
+cout<<"\n la suma de "<<x<<"+"<<y<<" es:"<<z;
+
+return 0;
+}
