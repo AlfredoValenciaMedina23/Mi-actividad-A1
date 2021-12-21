cebdd5789d44cc6fe1775ac314656f386189db71
Maria Mena <93356652+MariaMenaLino@users.noreply.github.com>
2021-12-20 19:19:15 -0500

    Add files via upload

---
+++ Actividad-A2/MenaLino Maria/utelvt.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,c=0,l;
+	float s=0,si=0, se=0;
+	cout<<"INGRESE EL LIMITE: ";
+	cin>>l;
+	hacer
+	{
+		cout<<"INGRESE EL SALDO: "<<c+1<<":";
+		cin>>x;
+		c=c+1;
+		s=s+x;
+		{
+			si (x>0)
+			{
+				si=si+x;
+			}
+			demas 
+			{
+			se=se+x;
+			}
+		}
+	}
+	mientras (c<l);
+	cout<<"INGRESOS SON: " <<si<<endl;
+	cout<<"EGRESOS SON: " <<se<<endl;
+	cout<<"SALDO RESTANTE: " <<s<<endl;
+	return (0);
+
+}
